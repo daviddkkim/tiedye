5912dbe45923a65d5e21df247a55703433abf7c0
Dong Kyun David Kim <dongkyunkim@Dongs-Air.fios-router.home>
2022-11-23 21:29:34 -0500

    convex init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import clientConfig from "../convex/_generated/clientConfig";
 import type { AppProps } from 'next/app'
 
+const convex = new ConvexReactClient(clientConfig);
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConvexProvider client={convex}>
+      <Component {...pageProps} />
+    </ConvexProvider>
+  )
 }
---
+++ pages/api/clicks.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ConvexHttpClient } from "convex/browser";
+import clientConfig from "../../convex/_generated/clientConfig";
+
+const convex = new ConvexHttpClient(clientConfig);
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const clicks = await convex.query("getCounter")("clicks");
+  res.status(200).json({ clicks });
+}
